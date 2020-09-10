d4082fc39586aac38fc80004ff51a401038b0753
Matias Napoli <mnap@pop-os.localdomain>
2020-09-10 16:41:41 -0300

    Add feathers-sequelize and further hooks support

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 // libraries
 const feathers = require('@feathersjs/feathers');
 const express = require('@feathersjs/express');
+//const socketio = require('@feathersjs/socketio');
 const {Sequelize, DataTypes} = require('sequelize');
+const fs = require('feathers-sequelize');
+
+/* config for postgres' DB connection URI string
+const conf = {
+  db: {
+    'user': 'username',
+    password: 'userpass',
+    host: 'localhost',
+    port: '5432',
+    name: 'dbname'
+  }
+}
+const dbinfo = conf.db;*/
 
 // new exp-feathers app instance
 const app = express(feathers());
 
 // new Sq connection instance
 const sequelize = new Sequelize(
-  'postgres://postgres:contrasenia@localhost:5432/fsp',
+  'postgres://postgres:PW@localhost:5432/fsp',
   {logging: false}
 );
 
 // seq models
 const Client = sequelize.define('client', {
   name: {type: DataTypes.STRING, allowNull: false}, //shorthand syntax: {name: DataTypes.STRING}
-  address: {type: DataTypes.STRING, defaultValue: "unspecified"}
+  address: {type: DataTypes.STRING, defaultValue: 'unspecified'}
   // id, createdAt and updatedAt are included and updated automatically (BUT NOT by direct SQL queries)
 },
   {} // model options (e.g: tableName, timestamps)
@@ -51,23 +65,6 @@ async function testSeq() {
   };
 };
 
-/* syncronize seq models with DB, meaning it will create the required tables (or add/alter missing/differing 
-* tables for such casses).
-* this is an example repository so 'force' and 'alter' options are setted to true, which specifies that it will dump and create 
-* again the tables specified by models.
-* Check Migrations for production phase: https://sequelize.org/master/manual/migrations.html */
-sequelize.sync({force: true, alter: true}).
-// fill DB with dummy models instances
-then(() => {
-  // use bulkCreate() for massive addition
-  Client.create({name: 'Ff', address: 'Gg'}).then(() => {
-    console.log('successful row insertion');
-  }).finally(() => {
-    sequelize.close();
-  });
-});
-
-
 // express deployment
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -75,78 +72,58 @@ app.use(express.static(__dirname));
 app.configure(express.rest());
 app.use(express.errorHandler());
 
-// feathers example service 
-class MessageService {
-  constructor() {
-    this.messages = [];
-  }
+// routes for services
+app.use('/clients', fs({Model: Client}));
+app.use('/estimates', fs({Model: Estimate}));
 
-  async find () {
-    return this.messages;
-  }
+const associateEstimateToClient = (context) => {
 
-  async create (data) {
-    const message = {
-      id: this.messages.length,
-      text: data.text
-    }
-
-    this.messages.push(message);
-
-    return message;
-  }
-}
-
-// new route for example service
-app.use('/messages', new MessageService());
-
-const createdAt = async context => {
-  context.data.text = new Date();
-  
-  return context;
 };
 
 // hooking example
-app.service('messages').hooks({
+app.service('estimates').hooks({
   before: {
-    create: [ createdAt ]
+    create: [ associateEstimateToClient ]
   }
 });
-
-
-/* //working on services example
-app.on('connection', connection =>
-  app.channel('everybody').join(connection)
-);
-
-app.service('messages').on('created', message => {
-  console.log('A new message has been created', message);
-});
-
-app.service('messages').create({
-  text: 'Hello world from the server'
-});*/
-
-// main func
-const main = async () => {
-  await app.service('messages').create({
-    text: 'Hello Feathers'
-  });
-
-  /*await app.service('messages').create({
-    text: 'Hello again'
-  });
-
-  const messages = await app.service('messages').find();
-
-  console.log('All messages', messages);*/
-};
 
 // start server
 app.listen(3030).on('listening', () =>
   console.log('Feathers server listening on localhost:3030')
 );
 
-// init testings
+// DB connection testings
 testSeq();
-main();
+
+/* syncronize seq models with DB, meaning it will create the required tables (or add/alter missing/differing 
+* tables for such casses).
+* this is an example repository so 'force' and 'alter' options are setted to true, which specifies that it will dump and create 
+* again the tables specified by models.
+* Check Migrations for production phase: https://sequelize.org/master/manual/migrations.html */
+sequelize.sync({force: true, alter: true})
+// fill DB with dummy models instances
+.then(() => {
+  Client.bulkCreate(
+    [ {name: 'Client 1', address: '1st Street'},
+    {name: 'Client 2', address: '2nd Street'},
+    {name: 'Client 3', address: '3rd Street'},
+    {name: 'Client 4', address: '4th Street'} ]
+  )
+/*}).then(() => {
+  Estimate.create({ammount: 165706.25});*/
+}).finally(() => {
+  console.log('successful rows insertion')
+});
+  
+/*Estimate.sync({force: true, alter: true})
+.then(() => {
+    try {
+      Estimate.create({ammount: 165706.25});
+    } catch (error) {
+      console.log('error inserting row @ Estimate table');
+    }
+  }).then(() => {
+      console.log('successful row insertion @ Estimate table');
+  }).finally(() => {
+    sequelize.close();
+});*/
